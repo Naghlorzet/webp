912339d105edc4034f94c0a0a7e61f31621f881b
Naghlorzet <141574192+Naghlorzet@users.noreply.github.com>
2023-08-22 09:25:20 +0800

    Update functions.js

--- functions.js
+++ functions.js
@@ -12,33 +12,33 @@ exports.getGroupAdmins = function (participants) {
 exports.updateDatabase = (jenis, after) => {
 	
     //DATABASE
-    const server = JSON.parse(fs.readFileSync('./database/server.json'))
-    const grups = JSON.parse(fs.readFileSync('./database/grups.json'))
+    const server = JSON.parse(fs.readFileSync('./server.json'))
+    const grups = JSON.parse(fs.readFileSync('./grups.json'))
     
     
         if (jenis == "url") {
       server[0].url = after
-    fs.writeFileSync('./database/server.json', JSON.stringify(server))
+    fs.writeFileSync('./server.json', JSON.stringify(server))
     return "success"
     } else if (jenis == "username") {
       server[0].username = after
-    fs.writeFileSync('./database/server.json', JSON.stringify(server))
+    fs.writeFileSync('./server.json', JSON.stringify(server))
     return "success"
       } else if (jenis == "password") {
       server[0].password = after
-    fs.writeFileSync('./database/server.json', JSON.stringify(server))
+    fs.writeFileSync('./server.json', JSON.stringify(server))
     return "success"
       } else if (jenis == "ip") {
       server[0].ip = after
-    fs.writeFileSync('./database/server.json', JSON.stringify(server))
+    fs.writeFileSync('./server.json', JSON.stringify(server))
     return "success"
       } else if (jenis == "domain") {
       server[0].domain = after
-    fs.writeFileSync('./database/server.json', JSON.stringify(server))
+    fs.writeFileSync('./server.json', JSON.stringify(server))
     return "success"
       } else if (jenis == "grup") {
       grups.push(after)
-    fs.writeFileSync('./database/grups.json', JSON.stringify(grups))
+    fs.writeFileSync('./grups.json', JSON.stringify(grups))
     return "success"
     
     
